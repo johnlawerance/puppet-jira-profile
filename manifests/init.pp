2d91f33a2c9e90926ac93d7907da3dd2ce05d313
John Clark <john@johnlawerance.com>
2015-11-29 13:33:28 -0600

    This seems wrong

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,3 +22,19 @@ class profile::jira {
   class { 'jira':}
   class { 'jira::facts': }
 }
+
+class profile::jira::db {
+
+  # Hiera lookups
+  $mysql_root_password = hiera('jira::db::root_password')
+
+  # Install MySQL, collect exports
+  class { '::mysql::server':
+    root_password           => "$mysql_root_password",
+    remove_default_accounts => true,
+    #override_options        => $override_options
+  }
+
+  Mysql::Db <<| tag == jira |>> 
+
+}
